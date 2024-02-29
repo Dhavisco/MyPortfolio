f83552378c3a8924ae192782b34223c6daea518e
Dhavisco <olaniyidavid2002@gmail.com>
2024-02-29 20:07:58 +0100

    styling

--- src/component/HomePage.jsx
+++ src/component/HomePage.jsx
@@ -7,14 +7,14 @@ const HomePage = () => {
   return (
     <div
       id="home"
-      className="text-s flex flex-col pt-8 px-4 items-center h-screen bg-cover font-[Poppins] bg-[#0a0a0a]"
+      className="text-s flex flex-col pt-8 px-4 items-center h-screen bg-cover font-[Poppins] bg-[#000000]"
     >
       <img
         src={img}
         alt="David Olaniyi"
         className="mx-auto w-[15rem] h-[20rem] object-cover border-gray-500 sm:w-48 sm:h-48 "
       />
-      <h1 className="text-2xl text-center mt-2 font-[Poppins] text-white font-extrabold">
+      <h1 className="text-2xl text-center mt-2.5 font-[Poppins] text-white font-extrabold">
         David Olaniyi
       </h1>
       <div className="mt-2 px-3 bg-gradient-to-r from-blue-600 to-gray-600 text-white sm:text-sm">
@@ -38,7 +38,7 @@ const HomePage = () => {
         />
       </div>
 
-      <div className="font-light text-sm text-gray-500 lg:text-xl  lg:w-4/5 py-3 leading-normal text-center flex justify-center flex-col items-center">
+      <div className="font-light text-xs text-gray-500 lg:text-xl  lg:w-4/5 py-3 leading-normal text-center flex justify-center flex-col items-center">
         I&apos;m a skilled frontend engineer creating efficient and
         user-friendly solutions to enhance productivity.
         <Socials />
