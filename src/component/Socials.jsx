380f060cdb27f410bcc67bfcabb0df1504e44d30
Dhavisco <olaniyidavid2002@gmail.com>
2024-02-29 16:40:31 +0100

    edited social link

--- src/component/Socials.jsx
+++ src/component/Socials.jsx
@@ -5,21 +5,7 @@ const Socials = () => {
     <div>
       <div className="flex py-4 space-x-6 sm:mt-0 leading-normal flex-center text-center">
         <a
-          href="https://facebook.com/sirmhuel.peter"
-          className=" text-gray-500 hover:text-blue-600"
-        >
-          <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
-            <path
-              fillRule="evenodd"
-              d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-              clipRule="evenodd"
-            />
-          </svg>
-
-          <span className="sr-only">Facebook page</span>
-        </a>
-        <a
-          href="https://instagram.com/bieefilled"
+          href="https://instagram.com/ayomi_dave"
           className="text-gray-500 hover:text-red-300"
         >
           <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
@@ -31,13 +17,19 @@ const Socials = () => {
           </svg>
           <span className="sr-only">Instagram page</span>
         </a>
-        <a href="" className="text-gray-500 hover:text-sky-500 ">
+        <a
+          href="https://twitter.com/olaniyidaviday1"
+          className="text-gray-500 hover:text-sky-500 "
+        >
           <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
             <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
           </svg>
           <span className="sr-only">Twitter page</span>
         </a>
-        <a href="" className="text-gray-500 hover:text-white ">
+        <a
+          href="https://github.com/dhavisco"
+          className="text-gray-500 hover:text-white "
+        >
           <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
             <path
               fillRule="evenodd"
@@ -49,10 +41,14 @@ const Socials = () => {
         </a>
 
         <a
-          href="https://www.linkedin.com/"
+          href="https://www.linkedin.com/in/dhavisco/"
           className="text-gray-500 hover:text-sky-500"
         >
-          <svg className="w-[18px] mt-0.5 h-[18px] rounded-xl" fill="currentColor" viewBox="0 0 24 24">
+          <svg
+            className="w-[18px] mt-0.5 h-[18px] rounded-xl"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+          >
             <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.761 0 5-2.239 5-5v-14c0-2.761-2.239-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.784 1.764-1.75 1.764zm12.5 12.268h-3v-5.604c0-1.337-.027-3.06-1.865-3.06-1.865 0-2.153 1.457-2.153 2.967v5.697h-3v-11h2.881v1.506h.041c.401-.762 1.381-1.565 2.841-1.565 3.041 0 3.604 2.003 3.604 4.609v6.45z" />
           </svg>
           <span className="sr-only">LinkedIn page</span>
